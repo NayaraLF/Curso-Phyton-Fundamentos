2f263599fc595f0fb495f2f5a0a3684d422a27f6
Nayara Louback-Franco <nayara993@gmail.com>
2025-07-16 15:12:31 -0300

    Update Modulo2.py

---
+++ scripts-phyton/Modulo2.py
@@ -0,0 +1,16 @@
+# Aula: Tipos primitivos e saída de dados
+
+num1 = int( input("Digite um numero"))
+num2 =int( input("Digite outro numero"))
+soma =  num1 + num2
+#print("A soma entre", num1, "e", num2, "é", soma)
+
+#formatado com f-string
+print(f"A soma entre {num1} e {num2} é {soma}")
+
+
+
+
+
+
+
