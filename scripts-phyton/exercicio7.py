9d34f717bb4ce0d28a7576b45f4b51eba7c14694
Nayara Louback-Franco <nayara993@gmail.com>
2025-07-17 11:31:23 -0300

    exercicios

--- scripts-phyton/exercicio7.py
+++ scripts-phyton/exercicio7.py
@@ -1,3 +1,3 @@
 
-valor = float(input("Digite um numero de medida em metros "))
+medida = float(input("Digite um numero de medida em metros "))
 print("Essa medida equivale a {} centimetros e {} milimetros.".format(valor*100, valor*1000))
--- scripts-phyton/exercicio8.py
+++ scripts-phyton/exercicio8.py
@@ -1,2 +1,4 @@
 num = int(input("Digite um numero inteiro "))
+print("-" *12)
 print("A tabuada desse numero é: \n 1 x {} = {} \n 2 x {} = {} \n 3 x {} = {}\n 4 x {} = {} \n 5 x {} = {} " .format(num, num*1, num, num*2, num, num*3, num, num*4, num, num*5))
+print("-" *12)
